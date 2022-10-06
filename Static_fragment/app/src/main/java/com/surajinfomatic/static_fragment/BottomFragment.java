a2df978918456599cc9541268db4397c9d2c3918
prof-vikas <vikas.mourya@protonmail.com>
2022-10-06 13:06:10 +0530

    Static Fragment

---
+++ Static_fragment/app/src/main/java/com/surajinfomatic/static_fragment/BottomFragment.java
@@ -0,0 +1,25 @@
+package com.surajinfomatic.static_fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BottomFragment extends Fragment {
+
+    private String mParam2;
+
+    public BottomFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bottom, container, false);
+    }
+}
